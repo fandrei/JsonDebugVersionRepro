abe0ab4c5ace7a23ed5f4bb4a63eec40dbd2517d
Andrei Faber <andrei.faber@gmail.com>
2021-08-25 15:18:19 -0500

    init

---
+++ ClassLibrary1/TestConverter1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Newtonsoft.Json;
+
+namespace ClassLibrary1
+{
+	public class TestConverter1<T> : JsonConverter
+	{
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			writer.WriteValue(value);
+		}
+
+		public override bool CanConvert(Type objectType)
+		{
+			var res = objectType == typeof(T);
+			return res;
+		}
+
+		public override bool CanRead => true;
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			var val = reader.Value;
+			return val;
+		}
+	}
+}
---
+++ JsonDebugVersionRepro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using ClassLibrary1;
+using ClassLibrary2;
+using Newtonsoft.Json;
+
+namespace JsonDebugVersionRepro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var settings = new JsonSerializerSettings
+			{
+				Formatting = Formatting.Indented,
+				DefaultValueHandling = DefaultValueHandling.Ignore,
+				NullValueHandling = NullValueHandling.Ignore,
+				Converters =
+				{
+					new TestConverter1<int>(),
+					new TestConverter2<int>(),
+				}
+			};
+
+			Debugger.Break();
+		}
+	}
+}
